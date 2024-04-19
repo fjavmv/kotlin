b6e903b56cd27655d13d34d59fbe0a14089b0ccc
Fjavier <fjavmvazquez@gmail.com>
2024-04-19 17:53:27 -0600

    ejemplo 1

---
+++ uno/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    //print("Hola desde Kotlin") Esto es un comentario
+    print("Enero\n")
+    println("Febrero")
+    println("Marzo")
+    println("Abril")
+    println("Mayo")
+    println("Junio")
+    println("Julio")
+    println("Agosto")
+    println("Septiembre")
+    println("Octure")
+    println("Noviembre")
+    println("Diciembre")
+}
