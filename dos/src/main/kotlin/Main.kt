a844d62ae6edc116888630b3bb650154519d2d03
Fjavier <fjavmvazquez@gmail.com>
2024-04-19 23:14:09 -0600

    KDoc

---
+++ dos/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+package fjavmvazquez.com
+
+/**
+ * @author Francisco Javier
+ * @param  x parametro x
+ * @param  y parametro y
+ * @sample: x=5 + y=6 = 11
+ */
+
+fun suma(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun main() {
+    val r = suma(5, 6)
+    println("El resultado de la suma es: $r")
+}
